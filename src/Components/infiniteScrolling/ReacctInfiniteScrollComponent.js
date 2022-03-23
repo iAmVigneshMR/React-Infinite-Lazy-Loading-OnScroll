e04c91300115d8c4f4f86edff65a6886abfd108c
Vignesh <vigneshmr@aggregateintelligence.in>
2022-03-23 17:00:31 +0530

    few changes

--- src/Components/infiniteScrolling/ReacctInfiniteScrollComponent.js
+++ src/Components/infiniteScrolling/ReacctInfiniteScrollComponent.js
@@ -1,40 +1,103 @@
+//first way
+
+// import axios from "axios";
+// import { Fragment, useEffect, useState } from "react";
+// import InfiniteScroll from "react-infinite-scroll-component";
+// import "./infiniteScrolling.css";
+
+// const ReacctInfiniteScrollComponent = () => {
+//     const [first, setfirst] = useState([]);
+//     const [count, setCount] = useState(0);
+//     const [hasMore, sethasMore] = useState(true);
+//     // const [first, setfirst] = useState({arrayPosts : []});
+//     const apiData = () => {
+//         axios.get("https://jsonplaceholder.typicode.com/posts")
+//             .then(res => {
+//                 let count2 = count + 10;
+//                 // console.log(typeof(count));
+//                 // console.log(typeof(count2));
+//                 // console.log(count2);
+//                 if(res.data.length === count){
+//                     sethasMore(false);
+//                 }
+//                 let arraySlice = res.data.slice(count,count2);
+//                 var joined = first.concat(arraySlice);
+//                 // arrayPosts.push(arraySlice);
+//                 // setfirst((arrayPosts)=> [...arrayPosts, arraySlice]);
+//                 setfirst(joined);
+//                 setCount(count2)
+//                 // console.log(res.data.length);
+//             })
+//             .catch(err => console.log(err));
+//     }
+//     useEffect(() => {
+//         apiData();
+//     }, [])
+//     // const handleCounter = () => {
+//     //     setCount(count + 10);
+//     // }
+
+//     return (
+//         <Fragment>
+//             <div className="inf-container">
+//                 <InfiniteScroll
+//                     dataLength={first.length}
+//                     // next={handleCounter}
+//                     next={apiData}
+//                     hasMore={hasMore}
+//                     loader={<h4 className='cardLoader'>Loading ...</h4>}
+//                     >
+//                 {first.length > 0 && first ? first.map((val) => {
+//                     return (
+//                             <div key={val.id}>
+//                                 {console.log(first.length)}
+//                                 <div>{val.id}</div>
+//                                 <div>{val.title}</div>
+//                                 <div>{val.body}</div>
+//                             </div>
+//                     )
+//                 }) : null}
+//                 </InfiniteScroll>
+//             </div>
+//         </Fragment>
+//     )
+// }
+
+// export default ReacctInfiniteScrollComponent
+
+//second way
+
 import axios from "axios";
 import { Fragment, useEffect, useState } from "react";
 import InfiniteScroll from "react-infinite-scroll-component";
-import { lazyload } from "react-lazyload";
 import "./infiniteScrolling.css";
 
 const ReacctInfiniteScrollComponent = () => {
     const [first, setfirst] = useState([]);
     const [count, setCount] = useState(0);
     const [hasMore, sethasMore] = useState(true);
-    // const [first, setfirst] = useState({arrayPosts : []});
+
     const apiData = () => {
-        axios.get("https://jsonplaceholder.typicode.com/posts")
+        let count2 = count + 10;
+        axios.get("https://jsonplaceholder.typicode.com/posts", {
+            params: {
+                _limit: count2
+            }
+        })
             .then(res => {
-                let count2 = count + 10;
-                // console.log(typeof(count));
-                // console.log(typeof(count2));
-                // console.log(count2);
-                if(res.data.length === count){
+                if (res.data.length === count) {
                     sethasMore(false);
                 }
-                let arraySlice = res.data.slice(count,count2);
+                let arraySlice = res.data.slice(count, count2);
                 var joined = first.concat(arraySlice);
-                // arrayPosts.push(arraySlice);
-                // setfirst((arrayPosts)=> [...arrayPosts, arraySlice]);
                 setfirst(joined);
-                setCount(count2)
-                // console.log(res.data.length);
+                setCount(count2);
             })
             .catch(err => console.log(err));
     }
     useEffect(() => {
         apiData();
     }, [])
-    // const handleCounter = () => {
-    //     setCount(count + 10);
-    // }
 
     return (
         <Fragment>
@@ -45,17 +108,17 @@ const ReacctInfiniteScrollComponent = () => {
                     next={apiData}
                     hasMore={hasMore}
                     loader={<h4 className='cardLoader'>Loading ...</h4>}
-                    >
-                {first.length > 0 && first ? first.map((val) => {
-                    return (
+                >
+                    {first.length > 0 && first ? first.map((val) => {
+                        return (
                             <div key={val.id}>
                                 {console.log(first.length)}
                                 <div>{val.id}</div>
                                 <div>{val.title}</div>
                                 <div>{val.body}</div>
                             </div>
-                    )
-                }) : null}
+                        )
+                    }) : null}
                 </InfiniteScroll>
             </div>
         </Fragment>
